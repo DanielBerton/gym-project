4dcc9f41d868fc6636a3444b50fe906efa1b3b13
DanielBerton <daniele.berton2@gmail.com>
2021-06-13 09:06:33 +0200

    Chore (Login): implemented ORM and logs

--- app.py
+++ app.py
@@ -5,46 +5,59 @@ from flask_login import login_required, current_user, login_manager, LoginManage
 from login import login_bp
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.register_blueprint(login_bp)
-engine = create_engine('sqlite:///gymdatabase.db', echo = True)
+
 metadata = MetaData()
-Session = sessionmaker(bind=engine)       # creazione della factory
-session = Session()
+
+# Session = sessionmaker(bind=engine)       # creazione della factory
+# session = Session()
 
 app.config['SECRET_KEY'] = 'ubersecret'
+
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+db = SQLAlchemy(app)
 
-class User(UserMixin):
-    # costruttore di classe
-    def __init__(self, id, email, pwd, active=True):
-        self.id = id 
-        self.email = email 
-        self.pwd = pwd
-        self.active = active
+class Users(db.Model, UserMixin):
+    id = db.Column('id', db.Integer, primary_key = True)
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(50))  
 
-def get_user_by_email(email): 
-    conn = engine.connect()
-    rs = conn.execute('SELECT * FROM users WHERE email = ?', email) 
-    user = rs.fetchone()
-    conn.close()
-    return User(user.id, user.email, user.pwd)
+    def __init__(self, id, email, password):
+        self.id = id
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return "<User(id='%s', email='%s', password='%s')>" % (self.id, self.email, self.password)
+    
+    # def get_id(self):
+    #     return self.id
+
+def get_user_by_email(email):
+    log('[get_user_by_email] executed', '')
+    user = Users.query.filter_by (email=email).first()
+    log('[get_user_by_email] user: ', user)
+    return user
 
 @login_manager.user_loader # definisce la callback
 def load_user(user_id):
-    conn = engine.connect()
-    rs = conn.execute('SELECT * FROM users WHERE id = ?', user_id) 
-    user = rs.fetchone()
-    conn.close()
-    return User(user.id, user.email, user.pwd)
+
+    log('[load_user] user_id: ', user_id)
+    user = Users.query.filter_by(id=user_id).first()
+    log('[load_user] Logged user: ',user)
+    return user
 
 
 @app.route('/')
 def home ():
+    log('[home] executed')
     # current_user identifica l'utente attuale 
     # # utente anonimo prima dell'autenticazione 
     log(current_user.is_authenticated)
@@ -55,32 +68,35 @@ def home ():
 @app.route('/private')
 @login_required # richiede autenticazione
 def private ():
-    conn = engine.connect()
-    users = conn.execute('SELECT * FROM users')
+    log('[private] executed')
+    users = Users.query.all()
+    log('[private] users ', users)
     resp = make_response(render_template("private.html", users=users))
-    conn.close()
     return resp
 
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
+    log('[logout] executed', '')
     logout_user()
     return redirect(url_for('home'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login ():
+    log('[login] executed')
     if request.method == 'POST':
-        conn = engine.connect()
-        rs = conn.execute('SELECT pwd FROM users WHERE email = ?', [request.form['user']]) 
-        real_pwd = rs.fetchone()
-        log(real_pwd)
-        conn.close()
-        if (real_pwd is not None):
-            log('OK ' + request.form['pwd']+ '  ' +real_pwd['pwd'])
-            if request.form['pwd'] == real_pwd['pwd']:
-                user = get_user_by_email(request.form['user'])
-                login_user(user)
+
+        email = request.form['user']
+
+        user = Users.query.filter_by(email=email).first()
+
+        log('[login] user: ',  user)
+        if (user and user.password is not None):
+            log('[login]', 'OK ' + request.form['password']+ '  ' +user.password)
+            if request.form['password'] == user.password:
+                users = get_user_by_email(request.form['user'])
+                login_user(users)
                 return redirect(url_for('private'))
         else:
             return redirect(url_for('home'))
@@ -89,15 +105,19 @@ def login ():
 
 @app.route('/test')
 def test():
-    u = request.args.get('user')
+    u = request.args.get('Users')
     #usafe
         #return "Welcome {}".format(u)
     #safe non viene interpretato come html
-    return render_template('profile.html', user=u)
+    return render_template('profile.html', Users=u)
 
 
 if __name__ == '__main__':
     app.run()
 
-def log(message):
-    print('Log message --> ', message)
+# Logger method
+# param message is optional
+def log(method, message=''):
+    print('---------------------------------')
+    print(method, message)
+    print('---------------------------------')
---
+++ generatedb.py
@@ -0,0 +1,42 @@
+from sqlalchemy import *
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+app = Flask (__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+metadata = MetaData()
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column('id', db.Integer, primary_key = True)
+    password = db.Column(db.String(50))  
+    email = db.Column(db.String(100))
+
+    def __init__(self, id, email, password):
+        self.id = id
+        self.password = password
+        self.email = email
+
+    def __repr__(self):
+        return "<User(id='%s', email='%s', password='%s')>" % (self.id, self.email, self.password)
+
+
+alice = Users(id=1, email='alice@gmail.com', password='alice')
+daniele = Users(id=2, email='daniele@gmail.com', password='daniele')
+
+db.create_all()
+
+db.session.add(alice)
+db.session.add(daniele)
+
+db.session.commit()
+
+users = Users.query.all ()
+
+
+print(db.engine.table_names())
+
+for u in users:
+    print (u)
